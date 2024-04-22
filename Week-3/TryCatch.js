de9c12923d1d0288240c840240ac87cfa95c056a
alamfaisal654 <alamfaisal654@gmail.com>
2024-04-22 21:53:34 +1000

    Latest week-8

--- Week-3/TryCatch.js
+++ Week-3/TryCatch.js
@@ -4,23 +4,23 @@
 const express = require("express");
 const res = require("express/lib/response");
 const app = express();
-const add = (nl, n2) => { return nl + n2; }
+const add = (n1, n2) => { return n1 + n2; }
 app.get("/add", (req, res) => {
     try {
-        const n1 = parseFloat(req.query.nl);
+        const n1 = parseFloat(req.query.n1);
         const n2 = parseFloat(req.query.n2);
-        if (isNaN(nl)) {
-            throw new Error("nl incorrectly defined");
+        if (isNaN(n1)) {
+            throw new Error("n1 incorrectly defined");
         }
         if (isNaN(n2)) {
             throw new Error("n2 incorrectly defined");
         }
-        const result = add(nl, n2);
+        const result = add(n1, n2);
         res.status(200).json({ statuscocde: 200, data: result });
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            statuscocde: 500, msg: error.tostring()
+            statuscocde: 500, msg: error.toString()
         })
     }
 });
